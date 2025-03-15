0c04f945b5448eca7aabfb3227dbf0b980d5ba62
Mamé Azad <mazad.contractor@libertyglobal.com>
2025-03-15 17:06:19 +0100

    basic jsx

---
+++ 1-level-foundation/app.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React element
+const jsxHeader = <h1>Hello wordl </h1>;
+console.log("jsx header", jsxHeader);
+
+// React Component
+const myName = "Azad";
+
+const Title = () => {
+  return (
+    <div>
+      <h1>This is title</h1>
+      {myName}
+    </div>
+  );
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {/** You can call component in many ways */}
+      <Title />
+      <Title></Title>
+      {Title()}
+      <h1>This is heading component</h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
